2c038755eb13aeeba37aeb02020d597f1a1b0841
Nikolas Vahlas <nikolasvahlas@nikolasvahlas.com>
2023-09-23 11:46:19 -0700

    update node_module and bcrypt

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models/user.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 router.post('/signup', async (req, res) => {
